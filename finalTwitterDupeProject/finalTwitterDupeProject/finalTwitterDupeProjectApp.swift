6318da7e2dc4c5464d2d8c5eaba319288804a3ea
StudentAM <studentam@Administrators-MacBook-Pro-2.local>
2024-05-15 10:20:33 -0700

    hola todos

---
+++ finalTwitterDupeProject/finalTwitterDupeProject/finalTwitterDupeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalTwitterDupeProjectApp.swift
+//  finalTwitterDupeProject
+//
+//  Created by StudentAM on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct finalTwitterDupeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
